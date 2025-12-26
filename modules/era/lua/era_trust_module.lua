daa239047f0e53151f465e6f9c2c500ef2cd612d
AlphieZ <Alphiez@users.noreply.github.com>
2025-12-25 19:54:29 -0500

    Add (for safety) Block Cipher Trade Logic

--- modules/era/lua/era_trust_module.lua
+++ modules/era/lua/era_trust_module.lua
@@ -3,6 +3,13 @@
 -----------------------------------
 require('modules/module_utils')
 -----------------------------------
+print('[TEST] no_trusts.lua loaded')
 local m = Module:new('era_no_trusts')
+
+-- (Backup) Disable trading Ciphers in Era servers.
+m:addOverride('xi.trust.onTradeCipher', function(player, trade, csid, rovCs, arkAngelCs)
+    return
+end)
+
 
 return m
